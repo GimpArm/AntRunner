c81cec5be5c77f21363b1694d27dd842fc43fe4a
Michal Sytko <michal.sytko@marc-cain.de>
2019-09-07 08:11:46 +0200

    Implement RunningMode (Play, Pause, NextStep)

---
+++ src/AntRunner/Models/GameRunningModeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntRunner.Models
+{
+    public enum GameRunningModeType
+    {
+        Playing,
+        Pause,
+        NextStep
+    }
+}
