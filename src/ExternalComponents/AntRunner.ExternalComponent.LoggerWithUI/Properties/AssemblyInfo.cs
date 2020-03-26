9d0d88f7001018d337f817ca8b6cc8b8df32e94c
Scott Downing <Scott.Downing@marc-cain.de>
2020-03-26 11:48:39 +0100

    Increase version to 1.2.4.0

--- src/ExternalComponents/AntRunner.ExternalComponent.LoggerWithUI/Properties/AssemblyInfo.cs
+++ src/ExternalComponents/AntRunner.ExternalComponent.LoggerWithUI/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Windows;
 [assembly: AssemblyTitle("AntRunner.ExternalComponent.LoggerWithUI")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.2.3.1")]
-[assembly: AssemblyFileVersion("1.2.3.1")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
 
